b3e6c387b7c48e53facdb49fb262ff461b82af5b
astarrf <1848208669@qq.com>
2023-11-18 00:03:12 +0800

    1

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+setup(
+    name='pymulti',
+    version='0.1.0',
+    packages=['PYMULTI-FOR-INUFSION-SIMULATION'],
+    install_requires=[
+        # 依赖库的列表
+        'os',
+        'shutil',
+        'subprocess',
+        're>=2.2.1',
+        'warnings'
+    ],
+)
