0e0bb25fdd8ce805ec4e7482ac701c8c1a8c3907
astarrf <1848208669@qq.com>
2023-11-24 21:34:53 +0800

    添加了用途说明

--- pymulti/__init__.py
+++ pymulti/__init__.py
@@ -1,4 +1,8 @@
-"""Multi-1D/Multi-2D/Multi-3D automaic control."""
+"""
+Multi-1D/Multi-2D/Multi-3D automaic control.
+本库由上海交通大学物理与天文学院开发，用于自动化控制等离子体模拟程序Multi-1D/Multi-2D/Multi-3D的运行。
+本库仅用于学术交流，不得用于商业用途。
+"""
 
 import os 
 import shutil
