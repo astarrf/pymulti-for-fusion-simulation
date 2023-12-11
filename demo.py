bd4261b4c8c211f5171328c8ff645b813528a617
astarrf <1848208669@qq.com>
2023-12-11 16:04:58 +0800

    补全了demo的内容

--- demo.py
+++ demo.py
@@ -19,3 +19,10 @@ my_opt = opt.BayesOptimizer(program='multi_3d',
                             tag='',
                             features=['x', 'y', 'z'],
                             func=my_func)
+res = my_opt.run(dimensions=[(-10.0, 10.0), (-20.0, 20.0),],
+                 delta=1e-3,
+                 print_step=False,
+                 do_delta_stop=True,
+                 n_calls=5000,
+                 random_state=0)
+ans = res.x
